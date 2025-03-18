7a85c06b5a5912818873b83eabe689745bb538ef
Ze40 <miko.r4r@gmail.com>
2025-03-18 16:35:59 +0300

    chore: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'], // Укажите пути к вашим HTML/JS-файлам
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5', // Кастомный цвет
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Кастомный шрифт
+      },
+    },
+  },
+  plugins: [],
+};
